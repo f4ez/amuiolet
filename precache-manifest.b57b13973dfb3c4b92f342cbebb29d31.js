b01e7288d6211f44f8d1b1db0261cd87933e0847
faez <faeztgh@users.noreply.github.com>
2020-09-14 16:25:53 +0430

    Updates

---
+++ precache-manifest.b57b13973dfb3c4b92f342cbebb29d31.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a778f6d9f5462ad0e7772f3aae4a0319",
+    "url": "/Amuiolet/index.html"
+  },
+  {
+    "revision": "9014728ba2e1f4a88bab",
+    "url": "/Amuiolet/static/css/main.3dc133c6.chunk.css"
+  },
+  {
+    "revision": "5b22c87594ccc3feecd4",
+    "url": "/Amuiolet/static/js/2.007b37a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Amuiolet/static/js/2.007b37a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9014728ba2e1f4a88bab",
+    "url": "/Amuiolet/static/js/main.cff14e4a.chunk.js"
+  },
+  {
+    "revision": "60cef750f58ea20c2792",
+    "url": "/Amuiolet/static/js/runtime-main.c874a2ca.js"
+  },
+  {
+    "revision": "75cfa85bc6848be511f54ddb1bcbf9b8",
+    "url": "/Amuiolet/static/media/Amuiolet.75cfa85b.svg"
+  },
+  {
+    "revision": "384896e586bbea82ec4a26c1ae3b1d08",
+    "url": "/Amuiolet/static/media/eclips.384896e5.png"
+  },
+  {
+    "revision": "d5812925c71048bffc720a245efbc58d",
+    "url": "/Amuiolet/static/media/girl-photo.d5812925.png"
+  }
+]);
